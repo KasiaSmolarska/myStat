29fcd938dd01b56c61b59f580a031071c6d46851
KasiaSmolarska <kokasmok7@gmail.com>
2017-08-17 19:45:31 +0200

    Ajax

--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,19 @@
 <h1>hello majap!</h1>
+
+<script>
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        
+        if(request.readyState == 4){
+
+           var text = request.responseText;
+           var arr = JSON.parse(text);
+
+           console.log(arr);
+        }
+
+    }
+
+    request.open('GET', 'http://localhost/majap/index.php?action=getTask', false); 
+request.send(null);
+</script>
