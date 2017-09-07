0cab6e56411d544c503a33dea41cfb4ce65562c8
KasiaSmolarska <kokasmok7@gmail.com>
2017-09-07 17:53:30 +0200

    przebudowa struktury szablonów po stronie php

--- templates/templates.php
+++ templates/templates.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="theme-color" content="#0296dc">
-    <title>Document</title>
-    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,300i,400,400i,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
-    <link rel="stylesheet" href="//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="templates/css/index.css">
-    <script src="templates/js/ejs.min.js"></script>
-    <script src="templates/js/ajax.js"></script>
+<?php
 
-
-    <?php $ejsTemplates = ["taskTemplate", "modalTemplate", "modalConfirm", "modalAlert", "addNewTask", "editTaskTemplate", "userData", "editUserData"] ?>
-
-    <?php foreach ($ejsTemplates as $templateName) : ?>
-        <noscript id="<?php echo $templateName ?>">
-        <?php include 'templates/ejs/'.$templateName.'.ejs' ?>
-        </noscript>
-
-    <?php endforeach ?>
-
-</head>
-<body>
-    <?php include 'templates/' . $htmlTemplate . '.html'?>
-
-    <div class="message">
-        <i style="font-size:30px; "class="mdi mdi-alert-outline"></i>
-    </div>
-</body>
-</html>
+if($htmlTemplate === 'login' ||  $htmlTemplate === 'register'){
+    include 'templates/login-register.php';
+} else{
+    include 'templates/panel.php';
+}
