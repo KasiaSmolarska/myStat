5bf93fbadf651dc9713435e5d349a45c28d43d59
KasiaSmolarska <kokasmok7@gmail.com>
2017-11-25 16:59:27 +0100

    dodawanie nagodzin

--- templates/js/overtime.js
+++ templates/js/overtime.js
@@ -1,12 +1,22 @@
-function addOvertime(){
+function addOvertime() {
 
     var data = {
-        description : document.getElementById('description').value,
-        client : document.getElementById('client').value,
-        day : document.getElementById('day').value,
-        dateFrom : document.getElementById('hourFrom').value + ":" + document.getElementById('minutesFrom').value,
-        dateTo : document.getElementById('hourTo').value + ":" + document.getElementById('minutesTo').value 
+        description: document.getElementById('description').value,
+        client: document.getElementById('client').value,
+        day: document.getElementById('day').value,
+        dateFrom: document.getElementById('hourFrom').value + ":" + document.getElementById('minutesFrom').value + ":00",
+        dateTo: document.getElementById('hourTo').value + ":" + document.getElementById('minutesTo').value + ":00"
     }
-      console.log(convertObjectToPostData(data));
+
+    var weekday = new Date(data.day).getDay();
+    if(weekday === 6 || weekday === 0){
+        data.weekend = 1;
+    }else{
+        data.weekend = 0;
+    }
+
+    ajax('addOvertime', function (response) {
+        console.log(response);
+    }, convertObjectToPostData(data))
 
 }
