9003522faebde10bc0279ab87eab85cc49cfe03d
KasiaSmolarska <kokasmok7@gmail.com>
2017-11-26 08:26:12 +0100

    add overtime finish

--- templates/js/overtime.js
+++ templates/js/overtime.js
@@ -1,3 +1,23 @@
+function onChangeHandler() {
+    console.time("abs");
+    var weekday = document.getElementById('day').value;
+    var weekend = checkWeekend(weekday);
+    var startTime = document.getElementById('hourFrom').value * 60 + parseInt(document.getElementById('minutesFrom').value);
+    var stopTime = document.getElementById('hourTo').value * 60 + parseInt(document.getElementById('minutesTo').value);
+    var calcHours = ((stopTime - startTime) / 60).toFixed(0);
+    if (weekend === 0) {
+        calcHours -= 8;
+    }
+    calcHours = addZero(calcHours);
+    var calcMinutes = parseInt((stopTime - startTime) % 60);
+    calcMinutes = addZero(calcMinutes);
+    var result = calcHours + ':' + calcMinutes;
+
+    var overtimeCount = document.getElementById("overtimeCount");
+    overtimeCount.innerHTML = result;
+    console.timeEnd("abs");
+}
+
 window.addEventListener("load", function () {
     var overtimeInputs = document.querySelectorAll(".timeField input");
 
@@ -6,18 +26,35 @@ window.addEventListener("load", function () {
             var input = overtimeInputs[i];
             input.value = '00';
 
-            input.addEventListener("change", function () {
-                var startTime = document.getElementById('hourFrom').value * 60 + parseInt(document.getElementById('minutesFrom').value);
-                var stopTime = document.getElementById('hourTo').value * 60 + parseInt(document.getElementById('minutesTo').value);
-                var calc = (stopTime - startTime) / 60;
-                var result = calc.toFixed(0) + ':' + parseInt((stopTime - startTime) % 60);
-
-                var overtimeCount = document.getElementById("overtimeCount");
-                overtimeCount.innerHTML = result;
-            })
+            input.addEventListener("change", onChangeHandler)
         }
+
+        document.getElementById('day').addEventListener("change", onChangeHandler)
+
     }
 })
+
+
+
+function addZero(calc) {
+    if (calc <= 0) {
+        calc = "00";
+    } else if (calc < 10) {
+        calc = "0" + calc;
+    }
+
+    return calc;
+}
+
+function checkWeekend(day) {
+
+    var weekday = new Date(day).getDay();
+    var weekend = 0;
+    if (weekday === 6 || weekday === 0) {
+        weekend = 1;
+    }
+    return weekend;
+}
 
 function addOvertime() {
 
@@ -29,12 +66,7 @@ function addOvertime() {
         dateTo: document.getElementById('hourTo').value + ":" + document.getElementById('minutesTo').value + ":00"
     }
 
-    var weekday = new Date(data.day).getDay();
-    if (weekday === 6 || weekday === 0) {
-        data.weekend = 1;
-    } else {
-        data.weekend = 0;
-    }
+    data.weekday = checkWeekend(data.day);
 
     ajax('addOvertime', function (response) {
         console.log(response);
